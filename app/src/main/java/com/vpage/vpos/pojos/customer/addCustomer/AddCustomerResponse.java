f4c3413ece33c9a902677492cbdccd5cee56cf81
VPageDevelopment <vpagedevelopment@gmail.com>
2017-05-19 17:43:47 +0530

    code update

--- app/src/main/java/com/vpage/vpos/pojos/customer/addCustomer/AddCustomerResponse.java
+++ app/src/main/java/com/vpage/vpos/pojos/customer/addCustomer/AddCustomerResponse.java
@@ -3,18 +3,26 @@ package com.vpage.vpos.pojos.customer.addCustomer;
 
 public class AddCustomerResponse {
 
-    private String status-code;
+    private String status_code;
 
     private String status;
 
     private String message;
 
-    public String getCode() {
-        return code;
+    public String getStatus_code() {
+        return status_code;
     }
 
-    public void setCode(String code) {
-        this.code = code;
+    public void setStatus_code(String status_code) {
+        this.status_code = status_code;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 
     public String getMessage() {
@@ -28,7 +36,8 @@ public class AddCustomerResponse {
     @Override
     public String toString() {
         return "AddCustomerResponse{" +
-                "code='" + code + '\'' +
+                "status_code='" + status_code + '\'' +
+                ", status='" + status + '\'' +
                 ", message='" + message + '\'' +
                 '}';
     }
